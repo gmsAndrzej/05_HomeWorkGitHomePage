b2c30bf946ae7f4f69f41dd248fc7c72c04d5e51
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-07-12 13:25:47 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Cześć wszystkim  !");
+let przycisk = document.querySelector(".przycisk");
+let nuda = document.querySelector(".nuda");
+
+przycisk.addEventListener("click", () => {
+
+
+    nuda.remove();
+
+
+});
+
+
+
+console.log(przycisk);
