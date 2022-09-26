609738d0915f0d2771b4d3f102bf277ecc8e7f70
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-09-26 13:13:45 +0200

    add og  icon

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
-console.log("Cześć wszystkim  !");
-let przycisk = document.querySelector(".przycisk");
-let nuda = document.querySelector(".nuda");
+{
+    const helloSay = () => {
 
-przycisk.addEventListener("click", () => {
+        console.log("Cześć wszystkim  !");
+    }
 
+    const checkLog = (onButton) => {
 
-    nuda.remove();
+        if (onButton.innerText === "Wyłącz nuda") console.log(`"Wyłącz nuda" ${onButton.innerText}`);
 
+        else console.log(`"Moja pasja" ${onButton.innerText}`);
 
-});
+    }
 
+    const init = () => {
 
+        changeButtonContent();
 
-console.log(przycisk);
+        helloSay();
+    }
+
+    const onChangeButtonHidden = () => {
+
+        document.querySelector(".nuda").toggleAttribute('hidden');
+
+    }
+
+    const changeButtonContent = () => {
+
+        const onButton = document.querySelector(".przycisk");
+        onButton.addEventListener("click", () => {
+
+            if (onButton.innerText === 'Wyłącz nuda') {
+
+                onButton.innerText = ("Moja pasja");
+                onButton.classList.toggle("kolorPrzycisk");
+
+            }
+
+            else {
+                ((onButton.innerText) = ("Wyłącz nuda"));
+                onButton.classList.toggle("kolorPrzycisk");
+            }
+
+            checkLog(onButton);
+            onChangeButtonHidden();
+
+        });
+    };
+
+    init();
+
+}
